cfda5b759fe26740b77745a31a14eca9b24eb77e
eashankumaaar00005-sudo <eashankumaaar00005@gmail.com>
2026-05-03 13:27:26 +0530

    Update intro.js

--- js/intro.js
+++ js/intro.js
@@ -4,22 +4,7 @@ const birthdayScreen = document.getElementById("birthdayScreen");
 const birthdayContent = document.querySelector(".birthday-content");
 const cinematicText = document.querySelector(".cinematic-text");
 const mainBtn = document.getElementById("mainBtn");
-const music = document.getElementById("bgMusic");
 
-if (music) {
-  music.volume = 0;
-  music.play().catch(()=>{});
-
-  let vol = 0;
-  const fade = setInterval(() => {
-    if (vol < 0.5) {
-      vol += 0.02;
-      music.volume = vol;
-    } else {
-      clearInterval(fade);
-    }
-  }, 200);
-  
 if (openBtn && lockScreen && birthdayScreen && birthdayContent && mainBtn) {
   openBtn.addEventListener("click", () => {
     lockScreen.classList.add("hide-lock");
@@ -43,19 +28,12 @@ if (openBtn && lockScreen && birthdayScreen && birthdayContent && mainBtn) {
       const candle = document.querySelector(".intro-cake-stage > .cake");
       if (candle) candle.classList.add("drop");
     }, 3900);
-setTimeout(() => {
-  const pop = document.getElementById("popSound");
-  if (pop) {
-    pop.currentTime = 0;
-    pop.volume = 0.6;
-    pop.play().catch(()=>{});
-  }
-}, 3900);
+
     setTimeout(() => {
-  const flash = document.getElementById("flash");
-  if (flash) flash.classList.add("active");
-}, 3900);
-    
+      const flash = document.getElementById("flash");
+      if (flash) flash.classList.add("active");
+    }, 3900);
+
     setTimeout(() => {
       const cakeStage = document.querySelector(".intro-cake-stage");
       if (cakeStage) cakeStage.classList.add("glow");
